2c6460eb5ecba25f30dcd0429bbb82cf9eed9e76
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2011-10-12 23:09:38 +0100

    Ooo I added a test!

---
+++ tests/arr.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Arr;
+
+/**
+ * Arr class tests
+ *
+ * @group Arr
+ * @group Arr
+ */
+class Tests_Arr extends \Fuel\Core\TestCase {
+    /*
+     * @test
+     */
+    public function test_1() {
+    }
+}
