deddbb922c6c5d60f314c7a2300f1d281b40bb35
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2011-10-26 12:05:40 +0100

    Added a test but can't be bothered running it

--- tests/arr.php
+++ tests/arr.php
@@ -9,9 +9,15 @@ namespace Arr;
  * @group Arr
  */
 class Tests_Arr extends \Fuel\Core\TestCase {
-    /*
+    /**
+	 * Test construction
+	 *
      * @test
      */
     public function test_1() {
+		$normal_array = array(1, 2, 3, 4, 5);
+		$arr = \Arr\Arr::forge($normal_array);
+
+		$this->assertCount($arr, 5);
     }
 }
